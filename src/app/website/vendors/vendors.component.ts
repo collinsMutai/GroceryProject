42a42094126d6e30b3b19db260f16476c56f562b
Collins Mutai <collinsfrontend@gmail.com>
2024-11-19 17:31:19 +0300

    ui

--- src/app/website/vendors/vendors.component.ts
+++ src/app/website/vendors/vendors.component.ts
@@ -1,12 +1,67 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-vendors',
   standalone: true,
-  imports: [],
   templateUrl: './vendors.component.html',
-  styleUrl: './vendors.component.css'
+  styleUrls: ['./vendors.component.css'],
+  imports: [CommonModule, FormsModule],
 })
 export class VendorsComponent {
+  products = [
+    {
+      name: 'Product 1',
+      price: 19.99,
+      date: '2023-11-01',
+      image:
+        'https://greenspoon.co.ke/wp-content/uploads/2021/12/Greenspoon-Flat-Parsley-Mlango-Farm1-720x480.jpg',
+    },
+    {
+      name: 'Product 2',
+      price: 29.99,
+      date: '2023-10-20',
+      image:
+        'https://greenspoon.co.ke/wp-content/uploads/2021/12/Greenspoon-Flat-Parsley-Mlango-Farm1-720x480.jpg',
+    },
+    {
+      name: 'Product 3',
+      price: 39.99,
+      date: '2023-10-15',
+      image:
+        'https://greenspoon.co.ke/wp-content/uploads/2021/12/Greenspoon-Flat-Parsley-Mlango-Farm1-720x480.jpg',
+    },
+  ];
 
+  selectedFilter = 'default';
+
+  filteredProducts = [...this.products];
+
+  applyFilter() {
+    let sortedProducts = [...this.products];
+
+    switch (this.selectedFilter) {
+      case 'priceAsc':
+        sortedProducts = sortedProducts.sort((a, b) => a.price - b.price);
+        break;
+      case 'priceDesc':
+        sortedProducts = sortedProducts.sort((a, b) => b.price - a.price);
+        break;
+      case 'newest':
+        sortedProducts = sortedProducts.sort(
+          (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+        );
+        break;
+      case 'oldest':
+        sortedProducts = sortedProducts.sort(
+          (a, b) => new Date(a.date).getTime() - new Date(b.date).getTime()
+        );
+        break;
+      default:
+        break;
+    }
+
+    this.filteredProducts = sortedProducts;
+  }
 }
